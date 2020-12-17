f99fde7edca566bc90106e732e5b0d62292a5cd2
Bartosz Świtalski <45360754+bartoszswitalski@users.noreply.github.com>
2020-12-17 14:21:35 +0100

    add department data

---
+++ Database/data_department.sql
@@ -0,0 +1,12 @@
+INSERT OR REPLACE
+    INTO department (name)
+    VALUES
+        ('engineering & technology'),
+        ('marketing & communications'),
+        ('business strategy'),
+        ('legal'),
+        ('facilities'),
+        ('sales, service & support'),
+        ('design'),
+        ('finance'),
+        ('people');
