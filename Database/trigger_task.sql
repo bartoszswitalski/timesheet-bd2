b8e90ad19df6288fbd03fab85e5c0082a731a1f8
mwiszenko <wiszenkom@gmail.com>
2020-12-17 14:30:06 +0100

    Rework of WHEN clause

--- Database/trigger_task.sql
+++ Database/trigger_task.sql
@@ -4,17 +4,23 @@ DROP TRIGGER IF EXISTS task_insert_tr;
 DROP TRIGGER IF EXISTS task_update_tr;
 
 CREATE TRIGGER IF NOT EXISTS task_insert_tr
-    BEFORE INSERT
+    AFTER INSERT
     ON task
-    WHEN NEW.user_id NOT IN (SELECT user_id FROM role_assignment WHERE user_id = NEW.user_id AND project_id = NEW.project_id)
 BEGIN
-    SELECT RAISE (ABORT, 'User is not assigned to the project.');
+    SELECT 
+        CASE
+            WHEN (NEW.user_id NOT IN (SELECT user_id FROM role_assignment WHERE user_id = NEW.user_id AND project_id = NEW.project_id))
+            THEN RAISE (ABORT, 'User is not assigned to the project.')
+            END;
 END;
 
 CREATE TRIGGER IF NOT EXISTS task_update_tr
-    BEFORE UPDATE
+    AFTER UPDATE
     ON task
-    WHEN NEW.user_id NOT IN (SELECT user_id FROM role_assignment WHERE user_id = NEW.user_id AND project_id = NEW.project_id)
 BEGIN
-    SELECT RAISE (ABORT, 'User is not assigned to the project.');
+    SELECT 
+        CASE
+            WHEN (NEW.user_id NOT IN (SELECT user_id FROM role_assignment WHERE user_id = NEW.user_id AND project_id = NEW.project_id))
+            THEN RAISE (ABORT, 'User is not assigned to the project.')
+            END;
 END;
