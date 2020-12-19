6ba88c98485d73656592dc5fed656187c0f27fdf
Bartosz Świtalski <45360754+bartoszswitalski@users.noreply.github.com>
2020-12-19 13:02:29 +0100

    update triggers

--- Database/triggers/trigger_role_assignment.sql
+++ Database/triggers/trigger_role_assignment.sql
@@ -1,7 +1,7 @@
 -- Role assignment triggers
-DROP TRIGGER IF EXISTS role_assignment_after_delete_tg;
+--DROP TRIGGER IF EXISTS role_assignment_after_delete_tg;
 
-CREATE TRIGGER IF NOT EXISTS role_assignment_after_delete_tg
+/*CREATE TRIGGER IF NOT EXISTS role_assignment_after_delete_tg
     AFTER DELETE
     ON role_assignment
 BEGIN
@@ -10,4 +10,4 @@ BEGIN
         WHEN (SELECT EXISTS(SELECT 1 FROM role_assignment WHERE project_id = OLD.project_id)) == 0 THEN NULL
     END
     WHERE user_id = OLD.user_id AND project_id = OLD.project_id;
-END;
+END;*/
